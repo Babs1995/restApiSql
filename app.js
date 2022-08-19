58d0c427f9c8b2ed1587f3b1e0bb101aea51e506
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-19 16:55:55 -0700

    Use Sequelize's authenticate function to test the database connection. A message should be logged to the console informing the user that the connection was successful

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
-'use strict';
+"use strict";
 
 // load modules
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 // variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+const enableGlobalErrorLogging =
+  process.env.ENABLE_GLOBAL_ERROR_LOGGING === "true";
 
 // create the Express app
 const app = express();
 
+// got code from Connecting to a database Instructions on https://sequelize.org/docs/v6/getting-started/
+const { Sequelize } = require("sequelize");
+
+// Option 2: Passing parameters separately (sqlite)
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "sjstd-restapi.db",
+});
+
+// Immediately Invoked Expression Using Async
+(async () => {
+  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to database was successful!");
+  } catch (error) {
+    console.log('There was an error connecting to the database.', error);
+    if (error.name === "SequelizeValidationError") {
+      const errors = error.errors.map((err) => err.message);
+      console.error("Validation errors: ", errors);
+    } else {
+      throw error;
+    }
+  }
+})();
+
 // setup morgan which gives us http request logging
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // setup a friendly greeting for the root route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Welcome to the REST API project!',
+    message: "Welcome to the REST API project!",
   });
 });
 
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
+    message: "Route Not Found",
   });
 });
 
@@ -40,9 +67,9 @@ app.use((err, req, res, next) => {
 });
 
 // set our port
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
 // start listening on our port
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
